7e885c6ea8bdb442ca68036701f6f15fc6d518ad
gustivirdila <gustivirdila_22@student.uns.ac.id>
2026-05-14 13:17:38 +0700

    fix: port for railway

--- node-version/server.js
+++ node-version/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-  res.send('Hello dari Node.js');
+  res.send('Hello dari Node.js 🚀');
 });
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' });
 });
 
-app.listen(3000, () => {
-  console.log('Server jalan di port 3000');
+app.listen(PORT, () => {
+  console.log(`Server jalan di port ${PORT}`);
 });
